afa1e38615794ba9f90273be00719aa3161ed9ea
Corey Coto <corey.coto@gmail.com>
2026-05-01 12:44:21 -0700

    git-slop: add 0.8.1 bottle.

--- Formula/git-slop.rb
+++ Formula/git-slop.rb
@@ -8,6 +8,12 @@ class GitSlop < Formula
       revision: "2f866fbdcdfeaba21c21f14ea622a4f81ee2ff9e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/coreycoto/homebrew-tap/releases/download/git-slop-0.8.1"
+    sha256 cellar: :any,                 arm64_tahoe:  "0b561366fe9413d4c04307ff8252c8b6b2fae0eb1088d889bd55d1ed57c37076"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f9509cfb818edc40cf7c7e1c71af531002a210e64312de5b3ea6bbbd9286b131"
+  end
+
   depends_on "rust" => :build
 
   depends_on "libyaml"
